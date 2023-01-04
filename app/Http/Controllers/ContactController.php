f5c626bb8046966a543ddafe307523d8e1f484bc
Carolina Praxedes <ana_carolina47@hotmail.com>
2023-01-04 19:25:34 -0300

    request finalizado

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -16,29 +16,48 @@ class ContactController extends Controller
     }
 
     public function saveContact(Request $request){
-        /*
+                    /*
         validação dos dados do formulário recebidos
         pelo o request. no request, quando os dados do usuário
         são recebidos no backend da aplicação os parametros são 
         associados de acordo com os names dos inputs 
         */
-        $request->validate([
-            /*o validate espera como parametro um array.
+                    /*o validate espera como parametro um array.
             nele informamos os campos que precisam ser validados e
             a chave que valida o tipo de validação vai ser
             o nome do input*/  
-            'name'=>'required|min:3|max:40',
+        
+        
+            /*quando a validação não passa, o laravel faz um
+            redirect para a rota anterior através a variavel 
+            $errors preenchida */ 
+
+        $request->validate([
+            'name'=>'required|min:3|max:40|unique:contact_sites',
             'phoneNumber'=>'required',
             'email'=>'email',
             'reasonContactId'=>'required',
             'message'=>'required|max:20000'
+        ],
+        [
+            'name.required'=>'O campo nome precisa ser preenchido',
+            'name.min'=>'O campo nome precisa ter no mínimo 3 caracteres',
+            'name.max'=>'O campo nome precisa no máximo de 40 caracteres',
+            'name.unique'=>'Esse nome já foi cadastrado',
 
-            /*quando a validação não passa, o laravel faz um
-            redirect para a rota anterior através a variavel 
-            $errors preenchida */ 
-        ]);
+            //em caso de uma grande quantidade de validações podemos 
+            //criar uma mensagem generica:
+            'required'=>'O campo :attribute deve ser preenchido',
+            //'required'=>'O campo deve ser preenchido'
+            'email.email'=>'O email informado não é valido',
+            'mensagem.max'=> 'A mensagem deve ter no max 2000 caracteres',
+            
+        ]
+    );
         //dd($request->all());
          ContactSite::create($request->all());
          return redirect()->route('site.index');
     }
+
+
 }
--- resources/views/site/layouts/_components/formContact.blade.php
+++ resources/views/site/layouts/_components/formContact.blade.php
@@ -2,10 +2,16 @@
 <form action="{{route('site.contact')}}" method="POST">
     @csrf
     <input name="name" value="{{old('name')}}" type="text" placeholder="Name" class="{{$classe}}">
+    @if($errors->has('name'))
+        {{-- caso existir um erro associado ao name --}}
+        {{$errors->first('name')}}
+    @endif
     <br>
     <input name="phoneNumber" value="{{old('phoneNumber')}}" type="text" placeholder="Phone Number" class="{{$classe}}">
+    {{$errors->has('phoneNumber') ? $errors->first('phoneNumber') : ''}}
     <br>
     <input name="email" value="{{old('email')}}" type="text" placeholder="E-mail" class="{{$classe}}">
+    {{$errors->has('email') ? $errors->first('email') : ''}}
     <br>
     
     <select name="reasonContactId"  class="{{$classe}}">
@@ -15,9 +21,12 @@
         @endforeach
 
     </select>
+    {{$errors->has('reasonContactId') ? $errors->first('reasonContactId') : ''}}
+
     <br>
     
     <textarea name="message"  class="{{$classe}}">{{(old('message')!= '') ? old('message') : 'Fill your message here' }}</textarea>
+    {{$errors->has('message') ? $errors->first('message') : ''}}
     <br>
     <button type="submit" class="{{$classe}}">SUBMIT</button>
 </form>
