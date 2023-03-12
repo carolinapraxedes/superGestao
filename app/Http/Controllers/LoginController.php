a7e457a7ba48e845df6f6ee47813e58ec8763637
Carolina Praxedes <ana_carolina47@hotmail.com>
2023-03-11 22:11:39 -0300

    Login feito

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -59,7 +59,7 @@ class LoginController extends Controller
 
             return redirect()->route('app.home');
         }else{
-            return redirect()->route('site.login',['erro'=>1]);
+            return redirect()->route('site.login');
         }
     }
 
